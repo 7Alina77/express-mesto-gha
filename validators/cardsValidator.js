94ac19a13afe0a644ca54a4723dbe068548cf098
Alina Potapchuk <alinapotapchuk7@gmail.com>
2023-04-30 19:42:57 +0300

    fix regexp 2.0

--- validators/cardsValidator.js
+++ validators/cardsValidator.js
@@ -1,10 +1,10 @@
 const { celebrate, Joi } = require('celebrate');
-const RegExp = require('../utils/constants');
+const regexp = require('../utils/constants');
 
 module.exports.validateCard = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().pattern(RegExp),
+    link: Joi.string().required().pattern(regexp),
   }),
 });
 
