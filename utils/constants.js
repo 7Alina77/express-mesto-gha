60619fb6ef447b988c03ce3b152a397e96241bd9
Alina Potapchuk <alinapotapchuk7@gmail.com>
2023-05-01 23:19:28 +0300

    fix npm

--- utils/constants.js
+++ utils/constants.js
@@ -1,2 +1,4 @@
-module.exports.SECRET_JWT_KEY = 'b05ac39769e78e4ce23d22b27149508674327458d46957834b0f7dc9a9c7a105';
-module.exports.regex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
+const SECRET_JWT_KEY = 'b05ac39769e78e4ce23d22b27149508674327458d46957834b0f7dc9a9c7a105';
+const regex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
+
+module.exports = { SECRET_JWT_KEY, regex };
