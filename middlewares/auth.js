bbd3e83d6c68916e0252f8604262d36b29d470fa
Alina Potapchuk <alinapotapchuk7@gmail.com>
2023-05-02 15:58:01 +0300

    fix of unauth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,15 +3,15 @@ const { SECRET_JWT_KEY } = require('../utils/constants');
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
 module.exports = (req, res, next) => {
-  const { token } = req.cookies;
+  const token = req.cookies.jwt;
   if (!token) {
-    return next(new UnauthorizedError('Авторизуйтесь'));
+    return next(new UnauthorizedError({ message: 'Авторизуйтесь' }));
   }
   let payload;
   try {
     payload = jwt.verify(token, SECRET_JWT_KEY);
   } catch (err) {
-    return next(new UnauthorizedError('Авторизуйтесь'));
+    return next(new UnauthorizedError({ message: 'Авторизуйтесь' }));
   }
   req.user = payload;
   return next();
