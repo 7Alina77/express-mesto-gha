736873ac80ab3f2c12cc5aa46d293a1807ec4470
Alina Potapchuk <alinapotapchuk7@gmail.com>
2023-05-02 18:26:43 +0300

    fix of validator in userSchema

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const validator = require('validator');
-// const UnauthorizedError = require('../errors/UnauthorizedError');
+const UnauthorizedError = require('../errors/UnauthorizedError');
 const reg = require('../utils/constants');
 
 const userSchema = new mongoose.Schema({
@@ -20,7 +20,9 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     validate: {
-      validator: (avatar) => reg.test(avatar),
+      validator(avatar) {
+        return reg.test(avatar);
+      },
       message: 'Ссылка введена некорректно',
     },
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
@@ -45,12 +47,12 @@ userSchema.statics.findUserByCredentials = function findUser(email, password) {
   return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
-        return Promise.reject(new Error('Неправильные почта или пароль'));
+        throw new UnauthorizedError('Неправильные почта или пароль');
       }
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            return Promise.reject(new Error('Неправильные почта или пароль'));
+            throw new UnauthorizedError('Неправильные почта или пароль');
           }
           return user;
         });
