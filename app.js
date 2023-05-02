63683a6714e47203abc1ecaa9a0279ef1c85beca
Alina Potapchuk <alinapotapchuk7@gmail.com>
2023-05-02 15:45:48 +0300

    fix of errors in app/js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(errors());
 app.use(routes);
+app.use(errors());
 
 app.listen(PORT);
