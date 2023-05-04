43868eb3f462962d49c63fc1d84598509828f558
Alina Potapchuk <alinapotapchuk7@gmail.com>
2023-05-04 22:05:28 +0300

    fix of else errors

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,7 @@ const { login, createUser } = require('../controllers/users');
 const auth = require('../middlewares/auth');
 const userRoutes = require('./users');
 const cardsRoutes = require('./cards');
-const NotFoundError = require('../errors/NotFoundError');
 const { validateLogin, validateSignUp } = require('../validators/userValidator');
-const { handleErrors } = require('../errors/handleErrors');
 
 router.post('/signin', validateLogin, login);
 router.post('/signup', validateSignUp, createUser);
@@ -15,9 +13,6 @@ router.use(auth);
 router.use('/users', userRoutes);
 router.use('/cards', cardsRoutes);
 
-router.use('*', (req, res) => {
-  const newNotFoundError = new NotFoundError('Нет данных');
-  handleErrors(newNotFoundError, res);
-});
+router.use('*', notFoudErrorForElse);
 
 module.exports = router;
